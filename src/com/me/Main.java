09ebe500b85e42af568baeedd0dda4d994a1f605
patrickatcw <pj8392@yahoo.com>
2018-02-28 22:27:03 -0500

    added instances and called methods, ran program, prinotu commented at bottom under step 33

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -10,11 +10,50 @@ but to access the object used in composition within the main class
 like saw in video
 */
 
+/*
+*****NOTE: composition is about having things, as opposed to being things;
+creating objects within objects.
+composition is most likely to be used before inheritance!!
+*/
 
-public class Main {
+//step 25 from checking all errors are gone in bedroom class step 24
+        public class Main {
 
-    public static void main(String[] args) {
+            public static void main(String[] args) {
 
-    }
+                //step 26 creating walls, creating instances
+                Wall wall1 = new Wall("West");
+                Wall wall2 = new Wall("East");
+                Wall wall3 = new Wall("South");
+                Wall wall4 = new Wall("North");
 
-}
+                //step 27 ceiling instance
+                Ceiling ceiling = new Ceiling(12, 55);
+
+                //step 28 bed instance
+                Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+                //step 29 lamp instance
+                Lamp lamp = new Lamp("Classic", false, 75);
+
+                //step 30 bedroom instance
+                Bedroom bedroom = new Bedroom("Patrick's", wall1, wall2, wall3, wall4,
+                        ceiling, bed, lamp);
+
+                //step 31 calling makeBed method
+                bedroom.makeBed();
+
+                //step 32 turn on lamp
+                bedroom.getLamp().turnOn();
+
+                //step 33 run program
+                //result
+                /*
+                Bedroom -> Making bed
+                Bed -> Making
+                Lamp -> Turning on
+                 */
+
+            }
+        }
+
