555201f031fe0970b4c4675ba6dc1b61f112f0cd
patrickatcw <pj8392@yahoo.com>
2018-02-28 22:03:46 -0500

    added ceiling class with fields, constructor, and getters, next class to construct is a bed class, step 14

---
+++ src/com/me/Ceiling.java
@@ -0,0 +1,26 @@
+package com.me;
+//from step 9 in wall class
+//step 10 make ceiling class
+public class Ceiling {
+
+    //step 11 fields
+    private int height;
+    private int paintedColor;
+
+    //step 12 constructor
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    //step 13 getters
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
+
+//next make the bed class, step 14
