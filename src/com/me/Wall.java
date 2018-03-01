fa9e205116a8c866489222ff18271cbce6d56b05
patrickatcw <pj8392@yahoo.com>
2018-02-28 21:59:46 -0500

    added wall class with fields, constructor, and getter, next class to construct is ceiling class, step 10

---
+++ src/com/me/Wall.java
@@ -0,0 +1,22 @@
+package com.me;
+//step 6 create Wall class
+//from bedroom class
+public class Wall {
+
+    //step 7
+    private String direction;
+
+    //step 8 make constructor
+    public Wall(String direction) {
+        this.direction = direction;
+
+    }
+
+    //step 9 make getter
+    public String getDirection() {
+        return direction;
+    }
+
+}
+
+//next make a ceiling class, step 10
