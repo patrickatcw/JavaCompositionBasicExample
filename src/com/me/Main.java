67d1976c5c5941a5308c846b06a082b412b9d124
patrickatcw <pj8392@yahoo.com>
2018-02-28 21:45:17 -0500

    set up basic structure, directions and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,20 @@
+package com.me;
+/*Directions
+- create a single room of a house using composition
+- think about the things that should be included in the room
+- maybe physical parts of the house but furniture as well
+- add at least one method to access an object via a getter and
+then that objects public method as you saw in previous video
+- then add at least one method to hide the object ex. not using  getter
+but to access the object used in composition within the main class
+like saw in video
+*/
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
